8f1776edf7d8377b12e05e8719b4bbb9086efa7e
Jinto Jose <jintoppy@gmail.com>
2015-05-02 14:06:45 +0530

    updating examples

--- examples/simple/public/index.js
+++ examples/simple/public/index.js
@@ -16,7 +16,6 @@
 'use strict';
 
 var Client = require('react-engine').client;
-var React = require('react');
 
 // Include all view files. Browerify doesn't do
 // this automatically as it can only operate on
@@ -25,8 +24,6 @@ require('./views/**/*.jsx', {glob: true});
 
 // boot options
 var options = {
-  react: React,
-
   // supply a function that can be called
   // to resolve the file that was rendered.
   viewResolver: function(viewName) {
