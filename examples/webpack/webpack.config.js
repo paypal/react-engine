d712cf182821e2130e87819b4a01fa1b784d14f7
Selvanathan, Sam <sjasel@gmail.com>
2015-06-30 15:32:05 -0700

    fix code style errors

--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -2,28 +2,28 @@
 
 module.exports = {
 
-    entry: './public/index.js',
+  entry: './public/index.js',
 
-    output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
-    },
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
 
-    module: {
-        loaders: [
-            {
-                //tell webpack to use jsx-loader for all *.jsx files
-                test: /\.jsx$/,
-                loader: 'jsx-loader?insertPragma=React.DOM&harmony'
-            },
-            {
-                test: /\.json$/,
-                loader: 'json-loader'
-            }
-        ]
-    },
+  module: {
+    loaders: [
+      {
+        //tell webpack to use jsx-loader for all *.jsx files
+        test: /\.jsx$/,
+        loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
 
-    resolve: {
-        extensions: ['', '.js', '.jsx', '.json']
-    }
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  }
 };
