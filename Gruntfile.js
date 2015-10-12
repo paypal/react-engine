23743e74135e87429a661e35fb7cfe3b6a2279c4
Gabriel Csapo <gacsapo@paypalcorp.com>
2015-10-12 11:53:08 -0700

    fixes code as per jscs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,10 @@
 'use strict';
 
-
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   require('time-grunt')(grunt);
   require('grunt-config-dir')(grunt, {
     configDir: require('path').resolve('grunt')
   });
 
-   grunt.registerTask('default', ['eslint']);
+  grunt.registerTask('default', ['eslint']);
 };
