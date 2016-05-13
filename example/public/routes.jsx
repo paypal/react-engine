9c06d404b72b66894efaefd88e81fc67818dfc78
Mark <mark@remarkablemark.org>
2016-05-13 01:41:48 -0400

    Use react-router `browserHistory` in example

--- example/public/routes.jsx
+++ example/public/routes.jsx
@@ -16,14 +16,15 @@
 'use strict';
 
 import React from 'react';
-import { Router, Route, IndexRoute, Redirect } from 'react-router';
+import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router';
 
 import Layout from './views/layout.jsx';
 import ListPage from './views/list.jsx';
 import DetailPage from './views/detail.jsx';
+import Error404 from './views/404.jsx';
 
-var routes = module.exports = (
-  <Router>
+module.exports = (
+  <Router history={browserHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={ListPage} />
       <Route path='/movie/:id' component={DetailPage} />
